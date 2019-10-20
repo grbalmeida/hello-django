1d32f71dac475b9691781a14c7a8c2e5e704b8bb
gilvan <g.r.almeida@live.com>
2019-10-20 19:51:34 -0300

    Creating the models

--- my_contacts/contacts/models.py
+++ my_contacts/contacts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, blank=True)
+    phone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    creation_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
