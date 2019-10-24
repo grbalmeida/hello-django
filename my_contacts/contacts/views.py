6cd015ee7d720dc204b3252d718762d091d5401e
gilvan <g.r.almeida@live.com>
2019-10-23 23:54:36 -0300

    Raising 404 errors

--- my_contacts/contacts/views.py
+++ my_contacts/contacts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Contact
 
 def index(request):
@@ -9,7 +9,7 @@ def index(request):
     })
 
 def see_contact(request, contact_id):
-    contact = Contact.objects.get(id=contact_id)
+    contact = get_object_or_404(Contact, id=contact_id)
 
     return render(request, 'contacts/see_contact.html', {
         'contact': contact
