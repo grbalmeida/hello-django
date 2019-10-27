1d6e069d6480b76a465d0a649cbf02e053303906
gilvan <g.r.almeida@live.com>
2019-10-26 22:39:34 -0300

    Pagination

--- my_contacts/contacts/views.py
+++ my_contacts/contacts/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Contact
 
 def index(request):
     contacts = Contact.objects.all()
+    paginator = Paginator(contacts, 2)
+    page = request.GET.get('p')
+    contacts = paginator.get_page(page)
 
     return render(request, 'contacts/index.html', {
         'contacts': contacts
