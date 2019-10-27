637ac1b5fc775c1dd3970160eeba7d088d0c52d5
gilvan <g.r.almeida@live.com>
2019-10-26 22:51:19 -0300

    Sorting and filtering values

--- my_contacts/contacts/views.py
+++ my_contacts/contacts/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, Http404
 from django.core.paginator import Paginator
 from .models import Contact
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.order_by('-id').filter(
+        show=True
+    )
+
     paginator = Paginator(contacts, 2)
     page = request.GET.get('p')
     contacts = paginator.get_page(page)
@@ -14,6 +17,9 @@ def index(request):
 
 def see_contact(request, contact_id):
     contact = get_object_or_404(Contact, id=contact_id)
+
+    if not contact.show:
+        raise Http404()
 
     return render(request, 'contacts/see_contact.html', {
         'contact': contact
