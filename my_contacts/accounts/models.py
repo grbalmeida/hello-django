541691183739f6b6723a737e4ebd5e206f1416e1
gilvan <g.r.almeida@live.com>
2019-10-27 21:44:32 -0300

    Models form

--- my_contacts/accounts/models.py
+++ my_contacts/accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django import forms
+from contacts.models import Contact
 
-# Create your models here.
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        exclude = ('show',)
--- my_contacts/accounts/views.py
+++ my_contacts/accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages, auth
 from django.core.validators import validate_email
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from .models import ContactForm
 
 def login(request):
     if request.method != 'POST':
@@ -89,4 +90,24 @@ def register(request):
 
 @login_required(redirect_field_name='login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    if request.method != 'POST':
+        form = ContactForm()
+
+        return render(request, 'accounts/dashboard.html', {
+            'form': form
+        })
+    
+    form = ContactForm(request.POST, request.FILES)
+
+    if not form.is_valid():
+        messages.error(request, 'Error submitting form')
+        form = ContactForm(request.POST)
+
+        return render(request, 'accounts/dashboard.html', {
+            'form': form
+        })
+
+    form.save()
+    messages.success(request, 'Contact successfully added')
+
+    return redirect('dashboard')
