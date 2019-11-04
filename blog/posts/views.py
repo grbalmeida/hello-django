0ef64b2e30f35c03bfa63530a0dcb58d545a0cf5
gilvan <g.r.almeida@live.com>
2019-11-03 22:30:33 -0300

    Return the number of post comments in the query

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
+from django.db.models import Q, Count, Case, When
 from .models import Post
 
 class PostIndex(ListView):
@@ -8,6 +9,19 @@ class PostIndex(ListView):
     template_name = 'posts/index.html'
     paginate_by = 3
     context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.order_by('-id').filter(post_published=True)
+        queryset = queryset.annotate(
+            number_of_comments=Count(
+                Case(
+                    When(comment__comment_published=True, then=1)
+                )
+            )
+        )
+
+        return queryset
 
 class PostSearch(PostIndex):
     pass
