8a7b8d034de2932467b1d3e4c848e982aae5a05a
gilvan <g.r.almeida@live.com>
2019-11-11 22:07:17 -0300

    Adding Product model

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    short_description = models.TextField(max_length=255)
+    long_description = models.TextField()
+    image = models.ImageField(upload_to='product_images/%Y/%m/', blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    price_marketing = models.FloatField()
+    promotional_price_marketing = models.FloatField(default=0)
+    product_type = models.CharField(
+        default='V',
+        max_length=1,
+        choices=(
+            ('V', 'Variation'),
+            ('S', 'Simple')
+        )
+    )
