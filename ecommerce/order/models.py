8c691b495b8fba15db7d5fbd0cb48f4aaf1ab9e1
gilvan <g.r.almeida@live.com>
2019-11-11 23:04:53 -0300

    Creating the models Order and OrderItem

--- ecommerce/order/models.py
+++ ecommerce/order/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.FloatField()
+    status = models.CharField(
+        max_length=1,
+        default='C',
+        choices=(
+            ('A', 'Approved'),
+            ('C', 'Created'),
+            ('D', 'Disapproved'),
+            ('P', 'Pending'),
+            ('S', 'Sent'),
+            ('F', 'Finished')
+        )
+    )
+
+    def __str__(self):
+        return f'Order Nº {self.pk}'
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=255)
+    product_id = models.PositiveIntegerField()
+    variation = models.CharField(max_length=50)
+    variation_id = models.PositiveIntegerField()
+    price = models.FloatField()
+    promotional_price = models.FloatField(default=0)
+    quantity = models.PositiveIntegerField()
+    image = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Order Item'
+        verbose_name_plural = 'Order Items'
+
+    def __str__(self):
+        return f'Order item {self.order}'
