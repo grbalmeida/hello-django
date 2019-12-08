0773030cbcbf4a0759587b3be008c4758998f0c1
gilvan <g.r.almeida@live.com>
2019-12-08 19:07:25 -0300

    Add to cart (part 1)

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
+from django.contrib import messages
 from . import models
 
 class ProductList(ListView):
@@ -19,7 +20,37 @@ class ProductDetails(DetailView):
 
 class AddToCart(View):
     def get(self, *args, **kwargs):
-        return HttpResponse('Add to Cart')
+        http_referer = self.request.META.get(
+            'HTTP_REFERER',
+            reverse('product:list')
+        )
+
+        variation_id = self.request.GET.get('vid')
+
+        if not variation_id:
+            messages.error(
+                self.request,
+                'Product not found'
+            )
+
+            return redirect(http_referer)
+
+        variation = get_object_or_404(models.Variation, id=variation_id)
+
+        if not self.request.session.get('cart'):
+            self.request.session['cart'] = {}
+            self.request.session.save()
+
+        cart = self.request.session.get('cart')
+
+        if variation_id in cart:
+            # TODO: Variation exists in cart
+            pass
+        else:
+            # TODO: Variation doesn't exist in cart
+            pass
+
+        return HttpResponse(f'{variation.product} {variation.name}')
 
 class RemoveFromCart(View):
     def get(self, *args, **kwargs):
